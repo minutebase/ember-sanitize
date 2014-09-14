01c9a15d924aff387a979ff5f206f4c483c598ee
Richard Livsey <richard@livsey.org>
2014-09-14 21:18:51 +0100

    import sanitize.js automatically

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  name: 'ember-sanitize'
+  name: 'ember-sanitize',
+
+  included: function(app) {
+    this._super.included(app);
+    this.app.import('bower_components/sanitize.js/lib/sanitize.js');
+  }
+
 };
