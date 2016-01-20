6332ebd2b68f89a6d0ffd6f42aa570038f18c289
orf <tom@tomforb.es>
2016-01-20 10:37:56 +0000

    Update sanitize-html.js

--- app/helpers/sanitize-html.js
+++ app/helpers/sanitize-html.js
@@ -1,4 +1,5 @@
 import { sanitize } from 'ember-sanitize/utils/sanitize';
+import getOwner from 'ember-getowner-polyfill';
 import Ember from 'ember';
 
 export default Ember.Helper.extend({
@@ -6,7 +7,7 @@ export default Ember.Helper.extend({
     let config, configName = params[1];
     if (configName) {
       //lookup the config
-      config = this.container.lookup('sanitizer:' + configName);
+      config = getOwner(this).lookup('sanitizer:' + configName);
     }
 
     let sanitized = sanitize(params[0], config);
