a781d1b61994db0b05014afa5561bae5c322c83d
Richard Livsey <richard@livsey.org>
2016-07-25 16:06:11 +0100

    Update to ember-cli 2.6.3

--- tests/acceptance/sanitize-test.js
+++ tests/acceptance/sanitize-test.js
@@ -1,39 +1,26 @@
-import Ember from 'ember';
-import startApp from '../helpers/start-app';
-
-var App;
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 
 // ApplicationController#html = "some <b>html</b> <i>here</i>"
-// as setup in dummy app
 
-function setup() {
-  App = startApp();
-}
-
-function teardown() {
-  Ember.run(App, 'destroy');
-}
-
-module('Acceptance: Sanitizing HTML', {
-  setup:    setup,
-  teardown: teardown
-});
+moduleForAcceptance('Acceptance | Sanitizing HTML');
 
 // {{sanitize-html html}}
-test("sanitizes HTML with default sanitizer if no config given", function() {
+test("sanitizes HTML with default sanitizer if no config given", function(assert) {
   visit("/");
   andThen(function() {
     var html = find("#sanitized-default").html();
-    ok(html.indexOf("some html here") !== -1, "contains sanitized HTML ("+html+")");
+    assert.ok(html.indexOf("some html here") !== -1, "contains sanitized HTML ("+html+")");
   });
 });
 
 
 // {{sanitize-html html "strict"}}
-test("looks up config on the container", function() {
+test("looks up config on the container", function(assert) {
   visit("/");
   andThen(function() {
     var html = find("#sanitized-with-config").html();
-    ok(html.indexOf("some html <i>here</i>") !== -1, "contains sanitized HTML ("+html+")");
+    assert.ok(html.indexOf("some html <i>here</i>") !== -1, "contains sanitized HTML ("+html+")");
   });
 });
+
--- tests/unit/utils/sanitize-test.js
+++ tests/unit/utils/sanitize-test.js
@@ -1,15 +1,15 @@
 import { sanitize } from 'ember-sanitize/utils/sanitize';
+import { module, test } from 'qunit';
 
 module('SanitizeHelper');
 
-test('sanitizes with the defaults', function() {
+test('sanitizes with the defaults', function(assert) {
   var result = sanitize("some <b>html</b> here");
-  equal(result, "some html here");
+  assert.equal(result, "some html here");
 });
 
-test('sanitizes with a custom config', function() {
+test('sanitizes with a custom config', function(assert) {
   var config = { elements: ['i'] };
   var result = sanitize("some <b>html</b> <i>here</i>", config);
-  equal(result, "some html <i>here</i>");
+  assert.equal(result, "some html <i>here</i>");
 });
-
