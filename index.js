26ec5b68f41b04078178344cc0940c3e044f3fd0
Dave Bouwman <dave@davebouwman.com>
2016-11-01 14:04:06 -0600

    uses pattern from ember-cli docs

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 module.exports = {
   name: 'ember-sanitize',
 
-  included: function(app) {
-    this._super.included(app);
-    this.app.import('bower_components/sanitize.js/lib/sanitize.js');
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+    this._super.included(target);
+    target.import('bower_components/sanitize.js/lib/sanitize.js');
   }
 
 };
